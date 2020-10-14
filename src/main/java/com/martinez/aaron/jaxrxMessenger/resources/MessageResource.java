0d4303971259511b77901852915fad9c7228060a
Aaron Martinez <aaronn.martinez@gmail.com>
2020-10-13 18:50:52 -0700

    Added addional functionality

--- src/main/java/com/martinez/aaron/jaxrxMessenger/resources/MessageResource.java
+++ src/main/java/com/martinez/aaron/jaxrxMessenger/resources/MessageResource.java
@@ -1,4 +1,4 @@
-package com.martinez.aaron.jaxrxMessenger.resources;
+ package com.martinez.aaron.jaxrxMessenger.resources;
 
 import java.util.List;
 
@@ -10,19 +10,21 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.martinez.aaron.jaxrxMessenger.model.Message;
 import com.martinez.aaron.jaxrxMessenger.service.MessageService;
 
 @Path("messages")
+// If every method consumes/produces the same media type, we can just annotate the whole class.
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 	MessageService service = new MessageService();
 	
 	
 	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	// Accept the Model type as argument to bind to the request body
 	// Jersey will know that this is what you are expecting
 	public Message addMessage(Message message) {
@@ -32,7 +34,6 @@ public class MessageResource {
 	@GET
 	// variable path
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
 	// Injects path param to parameter type.
 	public Message getMessage(@PathParam("messageId") long messageId) {
 		return service.getMessage(messageId);
@@ -44,22 +45,27 @@ public class MessageResource {
 	 * @return String that will be returned as a text/plain response.
 	 */
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Message> getMessages() {
+	public List<Message> getMessages(@QueryParam("year") int year, @QueryParam("start") int start,
+									 @QueryParam("size") int size) {
+		if(year > 0) {
+			return service.getAllMessagesForYear(year);
+		}
+		
+		if(start > 0 && size > 0) {
+			return service.getAllMessagesPaginated(start - 1, size);
+		}
+		
 		return service.getAllMessages();
 	}
 	
 	@DELETE
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message removeMessage(@PathParam("messageId") long messageId) {
 		return service.removeMessage(messageId);
 	}
 	
 	@PUT
 	@Path("/{messageId}")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message updateMessage(@PathParam("messageId") long id, Message message) {
 		message.setId(id);
 		return service.updateMessage(message);
